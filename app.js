7c1b8498fc84f9b39b4ac34c3bc27fa7bfbe07e0
Samuel Fernandez Munoz <smunoz@ebay.com>
2016-11-17 14:10:50 +0100

    Fixed compilation

--- app.js
+++ app.js
@@ -611,7 +611,7 @@ function sendMp(recipientId, message, metadata){
   searchApi.search(query).then(function(data){
     sendTypingOff(recipientId);
 
-    sendTotalResultButton(recipientId, 'Er zijn ' + data.totalResults + " resultaten op Marktplaats. Ga naar Marktplaats om alles te bekijken.', ,http://www.marktplaats.nl/z.html?query=" + encodeURI(query));
+    sendTotalResultButton(recipientId, 'Er zijn ' + data.totalResults + ' resultaten op Marktplaats. Ga naar Marktplaats om alles te bekijken.', 'http://www.marktplaats.nl/z.html?query=' + encodeURI(query));
 
     var items = data.results.map(function(result){
       return {
