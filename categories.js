2383c273655f55161cecb9aceb3a1054c66055c1
Samuel Fernandez Munoz <smunoz@ebay.com>
2016-11-17 17:11:07 +0100

    bug fix

--- categories.js
+++ categories.js
@@ -35,38 +35,8 @@ promise.then(function(cs) {
 });
 
 var getCategoryId = function(query){
-    return cats.find(function(c) { return c.name === query; }, {"categoryId": "0" }).categoryId;
+    return cats.find(function(c) { return c.name === query; }, {"id": "0" }).id;
 };
-
-// var getCategoryId = function(query){
-//     return new Promise(function(resolve, reject){
-//         if(!query){
-//             reject('no query term provided!');
-//         } else {
-//             var url = CATS_URL.replace(/API_TOKEN/, TOKEN);
-//             console.log(url);
-//             request(url)
-//                 .then(function(data){
-//                     data = JSON.parse(data);
-//
-//                     var cats = data._embedded["mp:category"].flatMap(function(l1cat) {
-//                         return l1cat._embedded["mp:category"].map(function(c) {
-//                             return { id: c.categoryId, name: c.name };
-//                         });
-//                     });
-//
-//                     var categoryId  = cats.find(function(c) {
-//                         return c.name === query;
-//                     }, { "categoryId": "0" }).categoryId;
-//
-//                     resolve(categoryId);
-//                 })
-//                 .catch(function(e){
-//                     reject(e);
-//                 })
-//         }
-//     });
-// };
 
 module.exports = {
     getCategoryId: getCategoryId
